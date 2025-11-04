0b952dc62aad86c1877ce904e797eaf1bfd56d79
sghosal <sghosal@chanzuckerberg.com>
2025-11-04 15:56:54 -0800

    Update notebooks, eqtl reproducibility results, remove stash

--- download_artifacts.py
+++ download_artifacts.py
@@ -90,6 +90,14 @@ ARTIFACTS = [
         local_path="f9bbc0ba.pq",
     ),
     Artifact(
+        remote_uri=f"s3://{DEFAULT_BUCKET}/gold_data/a0063c48.pq",
+        local_path="a0063c48.pq",
+    ),
+    Artifact(
+        remote_uri=f"s3://{DEFAULT_BUCKET}/gold_data/9a83db58.pq",
+        local_path="9a83db58.pq",
+    ),
+    Artifact(
         remote_uri=f"s3://{DEFAULT_BUCKET}/gold_data/924979a7.pq",
         local_path="924979a7.pq",
     ),
